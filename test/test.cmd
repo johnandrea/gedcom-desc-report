6555851a107985b4467aed8bbc43bc99c93b2ffc
johnandrea <46268748+johnandrea@users.noreply.github.com>
2026-02-02 17:08:14 -0400

    test files

---
+++ test/test.cmd
@@ -0,0 +1,2 @@
+gedcom-desc-report.py --title="Example report" --prep="John A." --dots=2 --person=12 family.ged >family.rtf 2>r.err
+"C:\Program Files\LibreOffice\program\swriter" --headless --convert-to pdf family.rtf
