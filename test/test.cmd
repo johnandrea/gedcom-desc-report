a281789a51e8ae685ead76dbd31d368131704f02
johnandrea <46268748+johnandrea@users.noreply.github.com>
2026-02-03 21:36:55 -0400

    update test output

--- test/test.cmd
+++ test/test.cmd
@@ -1,2 +1,13 @@
-gedcom-desc-report.py --title="Example report" --prep="John A." --dots=2 --person=12 family.ged >family.rtf 2>r.err
+@echo off
+
+if exist r.err del r.err
+if exist family.rtf del family.rtf
+if exist family.pdf del family.pdf
+
+rem note that the library path is relative to the script path
+
+..\gedcom-desc-report.py --title="Example report" --prep="John A." --dots=2 --person=12 --lib=..\readgedcom family.ged >family.rtf 2>r.err
+
+rem convert rft to pdf without interaction
+
 "C:\Program Files\LibreOffice\program\swriter" --headless --convert-to pdf family.rtf
